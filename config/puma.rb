2f52c37c3f9b1365b82f6b7de4b249ab3477ae8e
teenagelobotomist01-tech <teenagelobotomist01@gmail.com>
2025-10-28 11:03:48 -0500

    last

--- config/puma.rb
+++ config/puma.rb
@@ -1,12 +1,21 @@
-directory '/var/www/island/current'
-rackup "/var/www/island/current/config.ru"
-environment 'production'
+# config/puma.rb
 
-pidfile "/var/www/island/shared/tmp/pids/puma.pid"
-state_path "/var/www/island/shared/tmp/pids/puma.state"
-stdout_redirect "/var/www/island/shared/log/puma.stdout.log", "/var/www/island/shared/log/puma.stderr.log", true
+environment ENV.fetch("RAILS_ENV") { "development" }
 
-bind "unix:///var/www/island/shared/tmp/sockets/puma.sock"
+if ENV["RAILS_ENV"] == "development"
+  directory "#{Dir.pwd}"
+  port ENV.fetch("PORT") { 3000 }
+  pidfile "tmp/pids/puma.pid"
+  state_path "tmp/pids/puma.state"
+  stdout_redirect "log/puma.stdout.log", "log/puma.stderr.log", true
+else
+  rackup "/var/www/island/current/config.ru"
+  pidfile "/var/www/island/shared/tmp/pids/puma.pid"
+  state_path "/var/www/island/shared/tmp/pids/puma.state"
+  stdout_redirect "/var/www/island/shared/log/puma.stdout.log", "/var/www/island/shared/log/puma.stderr.log", true
+  bind "unix:///var/www/island/shared/tmp/sockets/puma.sock"
+end
+
 workers 2
 threads 1, 6
 preload_app!
