94a57638b5cec1d754ab8d5a163cea1fc423ab74
teenagelobotomist01-tech <teenagelobotomist01@gmail.com>
2025-10-29 01:44:38 +0000

    54.203.5.165

--- config/puma.rb
+++ config/puma.rb
@@ -1,19 +1,25 @@
 # config/puma.rb
 
-environment ENV.fetch("RAILS_ENV") { "development" }
+rails_env = ENV.fetch("RAILS_ENV", "development")
+environment rails_env
+puts ">>> Puma booting in ENV: #{rails_env}"
 
-if ENV["RAILS_ENV"] == "development"
+if rails_env == "development"
   directory "#{Dir.pwd}"
-  port ENV.fetch("PORT") { 3000 }
+  port ENV.fetch("PORT", 3000)
   pidfile "tmp/pids/puma.pid"
   state_path "tmp/pids/puma.state"
   stdout_redirect "log/puma.stdout.log", "log/puma.stderr.log", true
 else
-  rackup "/var/www/island/current/config.ru"
-  pidfile "/var/www/island/shared/tmp/pids/puma.pid"
-  state_path "/var/www/island/shared/tmp/pids/puma.state"
-  stdout_redirect "/var/www/island/shared/log/puma.stdout.log", "/var/www/island/shared/log/puma.stderr.log", true
-  bind "unix:///var/www/island/shared/tmp/sockets/puma.sock"
+  app_dir = "/var/www/island"
+  shared_dir = "#{app_dir}/shared"
+  current_dir = "#{app_dir}/current"
+
+  rackup "#{current_dir}/config.ru"
+  pidfile "#{shared_dir}/tmp/pids/puma.pid"
+  state_path "#{shared_dir}/tmp/pids/puma.state"
+  stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+  bind "unix://#{shared_dir}/tmp/sockets/puma.sock"
 end
 
 workers 2
