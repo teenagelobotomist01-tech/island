486982f5a9eebcf960336f16d5257ea500157198
teenagelobotomist01-tech <teenagelobotomist01@gmail.com>
2025-10-30 09:48:28 -0500

    Fix logger to write to production.log unless RAILS_LOG_TO_STDOUT is set

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,14 @@ Rails.application.configure do
 
   # Log to STDOUT with the current request id as a default log tag.
   config.log_tags = [ :request_id ]
-  config.logger   = ActiveSupport::TaggedLogging.logger(STDOUT)
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+  logger           = ActiveSupport::Logger.new(STDOUT)
+  logger.formatter = config.log_formatter
+  config.logger    = ActiveSupport::TaggedLogging.new(logger)
+else
+  config.logger = ActiveSupport::Logger.new("log/production.log")
+end
+
 
   # Change to "debug" to log everything (including potentially personally-identifiable information!)
   config.log_level = ENV.fetch("RAILS_LOG_LEVEL", "info")
