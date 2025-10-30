4392c18193e1cf781a12a01b5acaea2070d45227
teenagelobotomist01-tech <teenagelobotomist01@gmail.com>
2025-10-29 22:09:20 -0500

    Fix linked_files for production.key

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,11 @@ set :repo_url, "git@github.com:teenagelobotomist01-tech/island.git"
 set :deploy_to, "/var/www/island"
 set :branch, "main"
 
-set :linked_files, fetch(:linked_files, []).push("config/master.key", "config/database.yml")
+set :linked_files, fetch(:linked_files, []).push(
+  "config/master.key",
+  "config/credentials/production.key"
+)
+
 
 set :linked_dirs, fetch(:linked_dirs, []).push("log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system")
 
